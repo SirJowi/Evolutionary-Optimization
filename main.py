7ac5685472cb19741f8a08d04f982db476956447
SirJowi <johann-sinner@web.de>
2023-06-08 10:09:08 +0200

    + comments for to-do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """Beleg 3 - Modifizierte Evolutionsstrategie | 08/06/2023"""
 
-# zufälligen Startpunkt erstellen
+# zufälligen Startpunkt erstellen ()
 
 # Nebenbedingungen festlegen
 
@@ -8,10 +8,13 @@
 
 # Suchbereich festlegen
 
-# Normalverteilt Kinder erstellen
+# Normalverteilt Kinder erstellen (johann)
 
 # 20% regel
 
 # Kinder wegschmeißen, die NB verletzen
 
 # bestes Kind zu neuen Eltern machen
+
+# Visualisierung
+
