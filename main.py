8354c3da208a574a127321c0287a0c0b1beb09b4
goebphil <goeb.phil@hotmail.de>
2023-06-09 11:15:44 +0200

    Anpassungen

--- main.py
+++ main.py
@@ -22,13 +22,19 @@ nb[2, :] = [-np.pi, np.pi]        # Nebenbedingung in x3 Richtung
 
 # Definition des maximalen Suchbereiches
 maxD = np.ndarray(shape=(3,))
-maxD[0] = 10    # Suchbereich in x1 Richtung
-maxD[1] = 10    # Suchbereich in x2 Richtung
-maxD[2] = 10    # Suchbereich in x3 Richtung
+maxD[0] = 4    # Suchbereich in x1 Richtung
+maxD[1] = 4    # Suchbereich in x2 Richtung
+maxD[2] = 4    # Suchbereich in x3 Richtung
 
 
-# Definition der Optimierungsparameter
+runAnz = 500 # Anzahl der Iterationen
+c1 = 0.8
+c2 = 0.1
+n = 20
 
+
+zfHistory = []
+xHistory = np.zeros(shape=(runAnz, 3))
 
 
 # Zielfunktionswert berechnen
@@ -48,50 +54,94 @@ def zf(xVec):
 
 
 #1/5 Erfolg
-def Erfolg(A, x, n, maxD, c1):
+def Erfolg(Kinder, x, n, maxD, c1):
     xNeu = []
     k = 0
     for i in range(n):
-        #xNeu wird mit den Werten aus ZF befüllt
-        xNeu.append(zf(A[i,:]))
+        # xNeu wird mit den Werten aus ZF befüllt
+        xNeu.append(zf(Kinder[i, :]))
 
-        #Abspeichern von verbesserten Werten
+        # Abspeichern von verbesserten Werten
         if xNeu[i] > zf(x):
             k += 1
             print(k)
-    #Überprüfung der Erfolgsregel
-    if n/5 > k:
-        for j in range(k):#len(A)):
-            if (nb[0, 0] <= A[np.argmax(xNeu), 0] <= nb[0, 1] and nb[1, 0] <= A[np.argmax(xNeu), 1] <= nb[1, 1] and nb[2, 0] <= A[np.argmax(xNeu), 2] <= nb[2, 1]):
-                x[0] = A[np.argmax(xNeu),0]
-                x[1] = A[np.argmax(xNeu),1]
-                x[2] = A[np.argmax(xNeu),2]
-            #print("Erfolgsregel eingehalten")
+
+    # Überprüfung der Erfolgsregel
+    if n / 5 > k:
+        for j in range(k):
+            if (nb[0, 0] <= Kinder[np.argmax(xNeu), 0] <= nb[0, 1] and nb[1, 0] <= Kinder[np.argmax(xNeu), 1] <= nb[1, 1] and nb[2, 0] <= Kinder[np.argmax(xNeu), 2] <= nb[2, 1]):
+                x[0] = Kinder[np.argmax(xNeu), 0]
+                x[1] = Kinder[np.argmax(xNeu), 1]
+                x[2] = Kinder[np.argmax(xNeu), 2]
+                return maxD, x
+            # print("Erfolgsregel eingehalten")
             else:
-                if A[np.argmax(xNeu), 0] < nb[0, 0]:
-                    A[np.argmax(xNeu), 0] = nb[0, 0]
-                elif A[np.argmax(xNeu), 0] > nb[0, 1]:
-                    A[np.argmax(xNeu), 0] = nb[0, 1]
+                if Kinder[np.argmax(xNeu), 0] < nb[0, 0]:
+                    Kinder[np.argmax(xNeu), 0] = nb[0, 0]
+                elif Kinder[np.argmax(xNeu), 0] > nb[0, 1]:
+                    Kinder[np.argmax(xNeu), 0] = nb[0, 1]
 
-                if A[np.argmax(xNeu), 1] < nb[1, 0]:
-                    A[np.argmax(xNeu), 1] = nb[1, 0]
-                elif A[np.argmax(xNeu), 1] > nb[1, 1]:
-                    A[np.argmax(xNeu), 1] = nb[1, 1]
+                if Kinder[np.argmax(xNeu), 1] < nb[1, 0]:
+                    Kinder[np.argmax(xNeu), 1] = nb[1, 0]
+                elif Kinder[np.argmax(xNeu), 1] > nb[1, 1]:
+                    Kinder[np.argmax(xNeu), 1] = nb[1, 1]
 
-                if A[np.argmax(xNeu), 2] < nb[2, 0]:
-                    A[np.argmax(xNeu), 2] = nb[2, 0]
-                elif A[np.argmax(xNeu), 2] > nb[2, 1]:
-                    A[np.argmax(xNeu), 2] = nb[2, 1]
+                if Kinder[np.argmax(xNeu), 2] < nb[2, 0]:
+                    Kinder[np.argmax(xNeu), 2] = nb[2, 0]
+                elif Kinder[np.argmax(xNeu), 2] > nb[2, 1]:
+                    Kinder[np.argmax(xNeu), 2] = nb[2, 1]
 
-                x[0] = A[np.argmax(xNeu),0]
-                x[1] = A[np.argmax(xNeu),1]
-                x[2] = A[np.argmax(xNeu),2]
-
+                x[0] = Kinder[np.argmax(xNeu), 0]
+                x[1] = Kinder[np.argmax(xNeu), 1]
+                x[2] = Kinder[np.argmax(xNeu), 2]
+        return maxD, x
     else:
-        #print("Erfolgsregel nicht eingehalten")
+        # print("Erfolgsregel nicht eingehalten")
         maxD = maxD * c1
+        return maxD, x
 
-#Erfolg(A, x, n, maxD, c1)
+
+
+
+def plotResults(zfHistory, xHistory):
+    """
+
+    :param zfHistory:
+    :param xHistory:
+    :return: None
+    """
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].plot(zfHistory, 'rx-', markersize=5, linewidth=1)
+    ax[0].set_yscale('log')
+    ax[1].plot(xHistory[:, 0], xHistory[:, 1], 'bx-', markersize=5, linewidth=1)
+    ax[1].plot(1, 3, 'r.', markersize=3)
+    ax[0].grid()
+    ax[1].grid()
+    ax[0].set_title('Optimierungsfortschritt')
+    ax[0].set_ylabel('Zielfunktionswert')
+    ax[0].set_xlabel('Optimierungsiteration')
+    ax[1].set_title('Eingangsraumabsuche')
+    ax[1].set_ylabel(r'$x_2$')
+    ax[1].set_xlabel(r'$x_1$')
+    fig.tight_layout()
+
+    plt.show()
+
+
+
+
+
+for i in range(runAnz):
+    zfHistory.append(zf(x))
+    xHistory[i, :] = cp.deepcopy(x)
+
+    Kinder = normalverteilteKinder(c2,n, maxD, x)
+    maxD,x = Erfolg(Kinder, x, n, maxD, c1)
+    print(zf(x))
+
+plotResults(zfHistory, xHistory)
+
 
 
 
