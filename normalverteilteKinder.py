66ccb898c4bf6e1d58cc8f81e7401138a38047e5
SirJowi <johann-sinner@web.de>
2023-06-09 11:12:09 +0200

    + normKinder Subroutine

--- normalverteilteKinder.py
+++ normalverteilteKinder.py
@@ -13,7 +13,7 @@ def get_truncated_normal(Mittelwert=0, Standardabweichung=1, untereGrenze=-10, o
     )
 
 # Entscheide ob ich das Kind behalten soll, oder nicht. Behalten = True, Entfernen = False ----------------------------/
-def KindBehalten(x_vec_Kind):
+def KindBehalten(x_vec_Kind, minD):
     behalten = False
     for i in range(len(x_vec_Kind)):
         if x_vec_Kind[i] > minD or x_vec_Kind[i] < - minD:
@@ -23,32 +23,29 @@ def KindBehalten(x_vec_Kind):
     return behalten
 
 # Deklarieren ---------------------------------------------------------------------------------------------------------/
-C2      = 0.1  # minD / maxD
-n       = 20   # Anzahl an Kindern
-maxD    = 4    # Box maximal
-x_vec_Eltern     = np.array([2., 3., 1.])
+def normalverteilteKinder(C2 = 0.1, n = 20, maxD = 4, x_vec_Eltern = np.array([2., 3., 1.])):
+    # Initialisieren ------------------------------------------------------------------------------------------------------/
+    minD    = C2 * maxD    # Box minimal
+    x_vec_Kind_lokal = np.zeros(3)
+    x_vec_Kind_global= np.zeros(3)
+    Kinder = np.empty(3)    # Pro Zeile steht ein Kind (x1, x2, x3)
 
-# Initialisieren ------------------------------------------------------------------------------------------------------/
-minD    = C2 * maxD    # Box minimal
-x_vec_Kind_lokal = np.zeros(3)
-x_vec_Kind_global= np.zeros(3)
-Kinder = np.empty(3)    # Pro Zeile steht ein Kind (x1, x2, x3)
+    # Sammel 20 valide Kinder ---------------------------------------------------------------------------------------------/
+    while len(Kinder) < n:
+        # Lokales Kind erstellen ------------------------------------------------------------------------------------------/
+        for i in range(len(x_vec_Kind_lokal)):
+            x_i_Kind_rdm = get_truncated_normal(Mittelwert          = 0,
+                                                Standardabweichung  = maxD/3,
+                                                untereGrenze        = -maxD,
+                                                obereGrenze         = maxD
+                                                )
+            x_i_Kind_lokal = x_i_Kind_rdm.rvs()
+            x_vec_Kind_lokal[i] = x_i_Kind_lokal
 
-# Sammel 20 valide Kinder ---------------------------------------------------------------------------------------------/
-while len(Kinder) < n:
-    # Lokales Kind erstellen ------------------------------------------------------------------------------------------/
-    for i in range(len(x_vec_Kind_lokal)):
-        x_i_Kind_rdm = get_truncated_normal(Mittelwert          = 0,
-                                            Standardabweichung  = maxD/3,
-                                            untereGrenze        = -maxD,
-                                            obereGrenze         = maxD
-                                            )
-        x_i_Kind_lokal = x_i_Kind_rdm.rvs()
-        x_vec_Kind_lokal[i] = x_i_Kind_lokal
+        # Überprüfen, ob Kind innerhalb der Suchbox -----------------------------------------------------------------------/
+        if KindBehalten(x_vec_Kind_lokal, minD) == True:
+            x_vec_Kind_global = x_vec_Kind_lokal + x_vec_Eltern
+            Kinder = np.vstack([Kinder,x_vec_Kind_global])
+    return Kinder
 
-    # Überprüfen, ob Kind innerhalb der Suchbox -----------------------------------------------------------------------/
-    if KindBehalten(x_vec_Kind_lokal) == True:
-        x_vec_Kind_global = x_vec_Kind_lokal + x_vec_Eltern
-        Kinder = np.vstack([Kinder,x_vec_Kind_global])
-
-print(Kinder)
+print(normalverteilteKinder())
